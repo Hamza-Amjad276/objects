7524eb638b049101782f5838d9ccf524d3b43dcc
Hamza Amjad <hamzaharis298@gmail.com>
2024-07-10 19:34:29 +0500

    Add files via upload

---
+++ objects/main.js
@@ -0,0 +1,14 @@
+// // let student = {
+// //     name : "hamza",
+// //     class : 2,
+// //     grade : 9,
+// // }
+// // console.log(student.name);
+var person = {
+    name: "maryam",
+    facid: 4,
+    salary: 10000,
+    position: "teacher",
+    age: 15,
+    class: 9,
+};
---
+++ objects/main.ts
@@ -0,0 +1,80 @@
+// // let student = {
+// //     name : "hamza",
+// //     class : 2,
+// //     grade : 9,
+// // }
+// // console.log(student.name);
+
+// // let student1 = {
+// //     name : string,
+// //     age : number,
+// //     class : number,
+
+// // } = {
+// //     name : "hamza"
+// //     age : 15
+// //     class : 8
+// // }
+
+// type std = {
+//    name : string,
+//    age : number,
+//    class: number,
+
+// }
+
+// let std50 : std = {
+//     name : "hamza",
+//     age : 15,
+//     class : 9,
+
+
+// }
+
+// let std51 : std = {
+//     name : "ali",
+//     age : 12,
+//     class : 6,
+
+
+// }
+
+
+// union
+
+// let fname : String , number , boolean
+// fname = "hamza"
+// fname = 15
+// fname = false
+
+// let trafficlights : 'red' , 'green' , 'yellow'
+// trafficlights  = `red` 
+// trafficlights = `green`
+// trafficlights = `yellow`
+
+// intersection 
+
+// type teacher = {
+//     name : string
+//     facid : number
+//     salary : number
+//     position : string
+
+// }
+
+
+// type std = {
+//    name : string,
+//    age : number,
+//    class: number,
+
+// }
+
+// let person : teacher & std = {
+//     name : "maryam",
+//     facid : 4,
+//     salary : 10000,
+//     position : "teacher",
+//     age : 15,
+//     class : 9,
+// }
